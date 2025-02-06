46b14d79fd7439f21c57425e98833c4c96440f4f
JakdragonX <90160558+JakdragonX@users.noreply.github.com>
2025-02-06 14:02:35 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,22 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/courses",
+        destination: "/",
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
