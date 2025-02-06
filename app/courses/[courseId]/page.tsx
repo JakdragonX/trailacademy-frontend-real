7fb7883fe80b1aa186e76b8802b39f0d4d2d0cac
JakdragonX <90160558+JakdragonX@users.noreply.github.com>
2025-02-06 14:12:02 -0500

    Update page.tsx

--- app/courses/[courseId]/page.tsx
+++ app/courses/[courseId]/page.tsx
@@ -2,8 +2,8 @@
 
 import { useEffect, useState } from "react"
 import { useParams } from "next/navigation"
-import { CoursePreview } from "@/components/CoursePreview"
-import { LoadingState } from "@/components/LoadingState"
+import { CoursePreview } from "../../components/CoursePreview"
+import { LoadingState } from "../../components/LoadingState"
 
 export default function CoursePage() {
   const { courseId } = useParams()
