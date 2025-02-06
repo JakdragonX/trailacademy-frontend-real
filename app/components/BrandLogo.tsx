626a9882b5095ab496a10c65480fba4e4e5b80cd
JakdragonX <90160558+JakdragonX@users.noreply.github.com>
2025-02-06 13:18:58 -0500

    Delete app/components/BrandLogo.tsx

--- app/components/BrandLogo.tsx
+++
@@ -1,5 +0,0 @@
-import Image from "next/image"
-
-export default function BrandLogo() {
-  return <Image src="/logo.svg" alt="Trail Academy Logo" width={100} height={40} className="object-contain" />
-}
